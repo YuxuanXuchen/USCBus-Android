3ecf7a5a4a6259cfc5620102260aa0366821a8fa
Yuki Chen <chen859@usc.edu>
2018-06-05 10:21:53 -0700

    Android app src init

---
+++ app/src/main/java/com/uscbus/uscbus/Twitter.java
@@ -0,0 +1,21 @@
+package com.uscbus.uscbus;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class Twitter extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTitle("USC Buses Twitter");
+        setContentView(R.layout.activity_twitter);
+        WebView webview = new WebView(this);
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        setContentView(webview);
+        webview.loadUrl("https://uscbus.com/twitter.html");
+    }
+}
